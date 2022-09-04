ee8e2784f11e25659ac7e60e4ee79507457f06e8
Mountian Driver <lsfreigenberg@outlook.com>
2022-09-04 08:37:08 -0600

    schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,22 @@
 DROP DATABASE IF EXISTS  company_db;
-CREATE DATABSE company_db;
+CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department(
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30),
-)
+    name VARCHAR(30)
+);
 
-CREATE TABLE role(
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -24,10 +24,7 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES employee(id)
     ON DELETE SET NULL,
-    manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES manager(id),
-    ON DELETE SET NULL,
-)
+    manager_id INT REFERENCES employee(id)
+);
